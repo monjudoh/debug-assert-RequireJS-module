e26e2026de29239782bdcc030666070e33b6d1c2
monjudoh <monjudoh@gmail.com>
2010-11-08 21:52:38 +0900

    add licenses

--- src/debug.js
+++ src/debug.js
@@ -1,3 +1,13 @@
+/*
+* debug-assert-RequireJS-module - RequireJS module for debugging and assertion.
+*
+* https://github.com/monjudoh/debug-assert-RequireJS-module
+* version: 0.1
+*
+* Copyright (c) 2010 monjudoh
+* Dual licensed under the MIT (MIT-LICENSE.txt)
+* and GPL (GPL-LICENSE.txt) licenses.
+*/
 define(function() {
   var global = (function() {
     return this;
